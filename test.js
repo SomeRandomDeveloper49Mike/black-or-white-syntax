2597597b56e0199f7461b4b704c0177511f0f175
SomeRandomDeveloper49Mike <30830926+SomeRandomDeveloper49Mike@users.noreply.github.com>
2019-06-27 16:53:20 +0300

    Initial Commit

---
+++ test.js
@@ -0,0 +1,15 @@
+class Test {
+  constructor() {
+    if (10 == 10) {
+      console.log("Hello, World!")
+      let greet = (name) => {
+        this.alertAndLog("Hello, " + name);
+        return "Hello, " + name;
+      }
+    }
+  }
+  alertAndLog(foo) {
+    console.log(foo)
+    alert(bar)
+  }
+}
